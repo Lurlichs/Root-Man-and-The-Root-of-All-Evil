0ef0b54463c585ca01fb05e843c5fbd3acffb9bc
Chris Chua <someone42@gmail.com>
2023-02-03 22:32:14 +1100

    Implemented Roller controller and test scene

---
+++ Assets/Scripts/RollerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerController : MonoBehaviour
+{
+    public float acceleration = 0.5f;
+    public float maximumSpeed = 2.0f;
+
+    private bool isPositive;
+    private float currentVelocity;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Half go right, half go left
+        isPositive = Random.Range(0, 2) < 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        // Accelerate up to maximum speed in the chosen direction
+        float velocityChange = acceleration * Time.fixedDeltaTime;
+        if (Mathf.Abs(currentVelocity) < maximumSpeed)
+        {
+            if (isPositive)
+            {
+                currentVelocity += velocityChange;
+            }
+            else
+            {
+                currentVelocity -= velocityChange;
+            }
+        }
+        rb.velocity = new Vector3(currentVelocity, rb.velocity.y, rb.velocity.z);
+        rb.angularVelocity = new Vector3(0.0f, 0.0f, -currentVelocity);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if is a collision with floor
+        bool isFloor = true;
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            // If contact normal is not close to being "up", it's not the floor
+            float howCloseToUp = Vector3.Dot(contact.normal, Vector3.up);
+            if (howCloseToUp < 0.95f)
+            {
+                isFloor = false;
+            }    
+        }
+        if (!isFloor)
+        {
+            currentVelocity = 0.0f;
+            isPositive = !isPositive;
+        }
+    }
+}
