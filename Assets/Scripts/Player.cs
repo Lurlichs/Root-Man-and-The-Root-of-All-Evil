585454ffbe2ca4c4e979dd587dcb0afe27b2b454
David <davidchermanto@gmail.com>
2023-02-04 20:12:34 +1100

    Final stage balancing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -386,7 +386,7 @@ public class Player : MonoBehaviour
 
     public void Die()
     {
-        //UI_Manager.Instance.TurnOnDeathScreen();
+        UI_Manager.Instance.TurnOnDeathScreen();
     }
 
     public void RegenHealth()
@@ -407,7 +407,7 @@ public class Player : MonoBehaviour
         if (currentInvulnerabilityTime == 0 && !activatingShield)
         {
             currentHealth--;
-            //UI_Manager.Instance.UpdateHealthSetup();
+            UI_Manager.Instance.UpdateHealthSetup();
 
             if (currentHealth <= 0)
             {
