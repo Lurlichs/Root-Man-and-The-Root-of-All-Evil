c361a2f0608ac17cfdd68421fb1bac2f66d75d53
Chris Chua <someone42@gmail.com>
2023-02-04 20:57:36 +1100

    TreeBoss controller and prefab

---
+++ Assets/Scripts/TreeBossController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBossController : MonoBehaviour
+{
+    enum States
+    {
+        IDLE,
+        IDLE_SPECIAL,
+        LAUGH,
+        SPIKE,
+        SPIT
+    }
+
+    [Tooltip("Chance per idle animation cycle of doing the special idle animation")]
+    public float chanceOfSpecialIdle = 0.05f;
+    [Tooltip("Chance per idle animation cycle of doing a laugh")]
+    public float chanceOfLaugh = 0.05f;
+    [Tooltip("Chance per idle animation cycle of doing a spike attack")]
+    public float chanceOfSpike = 0.05f;
+    [Tooltip("Chance per idle animation cycle of doing a spit attack")]
+    public float chanceOfSpit = 0.05f;
+
+    private Animator animator;
+
+    private States currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // The animator is in a child object so we need to use GetComponentInChildren instead of GetComponent
+        animator = GetComponentInChildren<Animator>();
+        animator.SetTrigger("StartLaugh");
+        // Because the animator is in a child object, we can't get it to call a function here,
+        // instead, use AnimationEventsHandler to forward the calls to our AnimationClipEnded method
+        GetComponentInChildren<AnimationEventsHandler>().AddAnimationClipEndCallback(AnimationClipEnded);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void AnimationClipEnded(string clipName)
+    {
+        float randomVar = Random.value;
+
+        if (randomVar < chanceOfSpecialIdle)
+        {
+            animator.SetTrigger("StartSpecial");
+        }
+        else
+        {
+            randomVar -= chanceOfSpecialIdle;
+            if (randomVar < chanceOfLaugh)
+            {
+                animator.SetTrigger("StartLaugh");
+            }
+            else
+            {
+                randomVar -= chanceOfLaugh;
+                if (randomVar < chanceOfSpike)
+                {
+                    animator.SetTrigger("StartSpike");
+                }
+                else
+                {
+                    randomVar -= chanceOfSpit;
+                    if (randomVar < chanceOfSpit)
+                    {
+                        animator.SetTrigger("StartSpit");
+                    }
+                }
+            }
+        }
+    }
+}
